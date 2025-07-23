fb24742cdd7fc987465c92d0ef674c93ba8a2fdb
yambobkata123 <yambobiyam@gmail.com>
2025-07-23 20:09:30 +0300

    Added Firebase and NG Serve Working

---
+++ bookshare/src/app/environment.ts
@@ -0,0 +1,13 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "XXX",
+      authDomain: "XXX.firebaseapp.com",
+      projectId: "XXX",
+      storageBucket: "XXX.appspot.com",
+      messagingSenderId: "XXX",
+      appId: "XXX"
+    }
+  };
+  
