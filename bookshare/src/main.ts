6ff80c3d78e4dc61bbea9a9b1af8b20bc6edab6e
yambobkata123 <yambobiyam@gmail.com>
2025-08-21 23:10:54 +0300

    Update main.ts

--- bookshare/src/main.ts
+++ bookshare/src/main.ts
@@ -1,3 +1,4 @@
+import './polyfills';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
